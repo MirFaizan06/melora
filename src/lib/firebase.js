9371dd7fcee28ae332bd0dc09e30fb9ba9c9e3f8
Faizan <mirfaizan8803@gmail.com>
2025-05-25 01:17:49 +0530

    Initial commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,9 +9,9 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALG_4LHM7j-H6nP_O0tgEwng2Lwhm5VbU",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   authDomain: "melora-game-website.firebaseapp.com",
-  projectId: "melora-game-website",
   storageBucket: "melora-game-website.firebasestorage.app",
   messagingSenderId: "913087403813",
   appId: "1:913087403813:web:f083789014f18406caeccc",
