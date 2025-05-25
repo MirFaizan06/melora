8ba04a6e3b0223e2f9ac2f299140f9a4a5fb2d5c
Faizan <mirfaizan8803@gmail.com>
2025-05-25 16:25:06 +0530

    added more shop items

--- src/data/shopItems.js
+++ src/data/shopItems.js
@@ -6,15 +6,45 @@ export const shopItems = [
     type: "Skin",
   },
   {
+    id: "skin-2",
+    name: "Blossom Skin",
+    image: "/items/skin2.png",
+    type: "Skin",
+  },
+  {
     id: "theme-1",
     name: "Aurora Theme",
     image: "/items/theme1.png",
     type: "Theme",
   },
   {
-    id: "instrument-1",
+    id: "theme-2",
+    name: "Wodden Theme",
+    image: "/items/theme2.png",
+    type: "Theme",
+  },
+  {
+    id: "theme-3",
+    name: "Glass Theme",
+    image: "/items/theme3.png",
+    type: "Theme",
+  },
+  {
+    id: "key-1",
+    name: "Silver Keys",
+    image: "/items/keys1.png",
+    type: "Keys",
+  },
+  {
+    id: "key-2",
+    name: "Golden Keys",
+    image: "/items/key2.png",
+    type: "Keys",
+  },
+  {
+    id: "key-3",
     name: "Crystal Keys",
-    image: "/items/instrument1.png",
-    type: "Instrument",
+    image: "/items/key3.png",
+    type: "Keys",
   },
 ];
--- src/pages/Feedback.jsx
+++
@@ -1,4 +0,0 @@
-// Home.jsx
-export default function Home() {
-  return <div className="p-10 text-2xl">Feedback</div>;
-}
